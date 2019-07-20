239a7bc821ecc3cd58d6b489a180555a7f72cb71
Deepak Raja <deepuanirudh@gmail.com>
2019-07-20 23:12:29 +0530

    added files

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def results():
 
 
 #run(host="localhost",port=8997,debug=True)
-run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
+run(host="0.0.0.0",port=int(os.environ.get('PORT',5400)))
